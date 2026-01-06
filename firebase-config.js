024990dd5e149068833110bf0df06c758e9ebcf3
salihgundogan <salih.gundogan@istun.edu.tr>
2026-01-06 23:09:47 +0300

    🔧 Add firebase config for Netlify deployment

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase Configuration for Şimşek Kurye Dashboard
+window.FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBzdl0eyJMQ7MvbMEDsOAR6KA_fnlHTTwI",
+    authDomain: "simsek-kurye-dashboard.firebaseapp.com",
+    projectId: "simsek-kurye-dashboard",
+    storageBucket: "simsek-kurye-dashboard.firebasestorage.app",
+    messagingSenderId: "282514334592",
+    appId: "1:282514334592:web:6fcef1bb55f0d787cb1765"
+};
+
+window.FIREBASE_APP_ID = "simsek-kurye-dashboard";
